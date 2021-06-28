683494936b8d22a23d8acf1c32cf20bd304ac123
CarinaLR <carinalr84@gmail.com>
2021-06-28 09:34:43 -0400

    day 3 part II

--- day3TobogganTrajectory.js
+++ day3TobogganTrajectory.js
@@ -62,6 +62,21 @@ In this example, traversing the map using this slope would cause you to encounte
 
 Starting at the top-left corner of your map and following a slope of right 3 and down 1, how many trees would you encounter?
 
+---------- PART TWO------------------
+
+Time to check the rest of the slopes - you need to minimize the probability of a sudden arboreal stop, after all.
+
+Determine the number of trees you would encounter if, for each of the following slopes, you start at the top-left corner and traverse the map all the way to the bottom:
+
+Right 1, down 1.
+Right 3, down 1. (This is the slope you already checked.)
+Right 5, down 1.
+Right 7, down 1.
+Right 1, down 2.
+In the above example, these slopes would find 2, 7, 3, 4, and 2 tree(s) respectively; multiplied together, these produce the answer 336.
+
+What do you get if you multiply together the number of trees encountered on each of the listed slopes?
+
 */
 
 const test = [
@@ -96,7 +111,6 @@ const tobogganTrajectory = (arr) => {
   let count = 0;
   for (let i = 0; i < arr.length - 1; i++) {
     let next = arr[i + 1][i * 3 + 3];
-    //console.log('next', next, i + 1, (i * 3) + 3, "length", arr[i].length)
     if (next === "#") {
       count++;
     }
@@ -115,10 +129,52 @@ const helper = (row, targetPosition) => {
   }
 
   return concatRow[targetPosition] == "#" ? 1 : 0;
-};
+}; //247.
 
 //console.log(tobogganTrajectory(test));
-//console.log('=====')
 //console.log(tobogganTrajectory(puzzle));
-// console.log(tobogganTrajectory(test2));
-//console.log(puzzle);
+
+//----> PART II <----//
+
+const eachPuzzle = puzzle.map((line) => [...line]);
+
+const tobogganTrajectory2 = (arr) => {
+  let multy_trees = 1;
+  let slopes = [
+    [1, 1],
+    [3, 1],
+    [5, 1],
+    [7, 1],
+    [1, 2],
+  ];
+
+  for (slope of slopes) {
+    multy_trees *= findTrees(arr, slope[0], slope[1]);
+  }
+  return multy_trees;
+};
+
+const findTrees = (arr, sX, sY) => {
+  let x = 0;
+  let y = 0;
+  let count_trees = 0;
+
+  while (y < getLength(arr)) {
+    const current = getCoordinates(arr, x, y);
+    if (current === "#") count_trees++;
+    x += sX;
+    y += sY;
+  }
+  return count_trees;
+};
+
+const getCoordinates = (arr, x, y) => {
+  return arr[y][x % arr[0].length];
+};
+
+const getLength = (lines) => {
+  return lines.length;
+}; //2983070376
+
+//console.log(tobogganTrajectory2(test));
+console.log(tobogganTrajectory2(eachPuzzle));
